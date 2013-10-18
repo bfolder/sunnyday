cf885947360f62ae104ce146576a25bd2a5fe5b4
Heiko Dreyer <mail@boxedfolder.com>
2013-10-18 23:26:47 +0200

    Added configuration file

--- tests/general_tests.rb
+++ tests/general_tests.rb
@@ -5,15 +5,15 @@ class TestCaseGeneral < Test::Unit::TestCase
     @client = SunnyDay.client
     assert_not_nil(@client, 'Client should not be nil')
 
-    @client.configure do |client|
-      client.app_id = 21311323
+    @client.configure do |config|
+      config.app_id = 21311323
     end
 
-    assert_equal(@client.app_id, 21311323, 'App Id should equal 21311323')
+    assert_equal(@client.config.app_id, 21311323, 'App Id should equal 21311323')
 
-    @client = SunnyDay.client(21311323)
+    @client = SunnyDay.client(app_id: 21311323)
 
     assert_not_nil(@client, 'Client should not be nil')
-    assert_equal(@client.app_id, 21311323, 'App Id should equal 21311323')
+    assert_equal(@client.config.app_id, 21311323, 'App Id should equal 21311323')
   end
 end
