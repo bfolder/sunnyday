3e5788c8ba06f485b6c41bd3a608891fab2656f5
Heiko Dreyer <mail@boxedfolder.com>
2013-10-19 18:57:56 +0200

    All test cases are now in a single place

--- test/test.rb
+++ test/test.rb
@@ -3,8 +3,6 @@ $LOAD_PATH << File.expand_path( File.dirname(__FILE__) + '/../lib' )
 require 'test/unit'
 require 'sunnyday'
 
-require_relative 'api_test'
-
 class TestCaseGeneral < Test::Unit::TestCase
   def test_client_configuration
     @client = SunnyDay.client
@@ -21,5 +19,31 @@ class TestCaseGeneral < Test::Unit::TestCase
     assert_equal('de', @client.config.language, 'Language should be de')
     assert_equal('1.5', @client.config.api_version, 'Api version should be 1.5')
     assert_equal('imperial', @client.config.units, 'Units should be imperial')
+  end
+
+  def setup
+    @client = SunnyDay.client
+  end
+
+  def test_current_weather
+    results = @client.current_weather(q: 'London, UK')
+    assert_equal('London', results[:name], 'Should be London')
+  end
+
+  def test_forecast
+    results = @client.forecast(q: 'London,UK')
+    assert_equal('London', results[:city][:name], 'Should be London')
+  end
+
+  def test_daily_forecast
+    results = @client.daily_forecast(q: 'London,UK', cnt: 7)
+    assert_equal('London', results[:city][:name], 'Should be London')
+    assert_equal(7, results[:list].size, 'This should be 7 days forecast')
+  end
+
+  def test_find
+    results = @client.find(q: 'London,UK')
+    assert(results[:list].size > 0, 'There should be at least one result')
+    assert_equal('London', results[:list][0][:name], 'Should be London')
   end
 end
